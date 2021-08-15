516ea0253dbb42647972ceddf06bbac2ad1be9d7
eilla1 <git@ella.cx>
2021-08-14 19:11:59 -0700

    add images and some placeholder things

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,72 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Plx from "react-plx";
+
+const parallaxData = [
+  {
+    start: 0,
+    end: 500,
+    properties: [
+      {
+        startValue: 0,
+        endValue: -200,
+        property: "translateY",
+      },
+    ],
+  },
+];
+
+const layer2 = [
+  {
+    start: 0,
+    end: 500,
+    properties: [
+      {
+        startValue: 0,
+        endValue: 100,
+        property: "translateY",
+      },
+    ],
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>z</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <h1 className="text-2xl font-bold text-center">hi</h1>
+        <div className="relative">
+          <img src="/valley/0.png" alt="valley" className="absolute" />
+          <img src="/valley/1.png" alt="valley" className="absolute" />
+          <img src="/valley/2.png" alt="valley" className="absolute" />
+          <img src="/valley/3.png" alt="valley" className="absolute" />
+          <img src="/valley/4.png" alt="valley" className="absolute" />
+          <Plx parallaxData={layer2}>
+            <img src="/valley/5.png" alt="valley" className="absolute" />
+          </Plx>
+          <Plx parallaxData={parallaxData}>
+            <img src="/valley/6.png" alt="valley" className="absolute" />
+          </Plx>
+        </div>
+        <div className="bg-red-900 relative mt-96">
+          <h1>hi</h1>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
+          <p>hihihihi</p>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
